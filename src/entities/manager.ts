3890b347ad099eeaee31e7b001950da269eed9bb
Mario Schmidt <marioschmidt@edgestream.de>
2022-11-04 11:21:47 +0100

    #2 Subscribe to "detached" and "merged" events for a single managed entity

--- src/entities/manager.ts
+++ src/entities/manager.ts
@@ -1,4 +1,4 @@
-import { createEmitter, EventSource } from "../events"
+import { createEmitter, EventSource, EventMessage, EventListener } from "../events"
 
 export type KeyGenerator = <T extends object>(entity: Partial<T>) => any
 export type EntityGenerator = <T extends object>(manager: EntityManager, entity: T, key: any) => T
@@ -18,7 +18,14 @@ export interface EntityManager {
     attach<T extends object>(entity: T): T
     detach<T extends object>(entity: T): void
     find<T extends object>(key: any): T | undefined
-    merge<T extends object>(key: any, changes: Partial<T>): void
+    merge<T extends object>(entity: T, changes: Partial<T>): void
+    subscribe<T extends object>(entity: T): Subscription
+    unsubscribe(subscription: Subscription): void
+}
+export interface Subscription {
+    detached: EventSource<DetachedEventData, this>
+    merged: EventSource<MergedEventData, this>
+    unsubscribe(): void
 }
 export function createManager(options: ManagerOptions) {
     return new EntityManagerImpl(options)
@@ -32,6 +39,7 @@ class EntityManagerImpl implements EntityManager {
     #keyGenerator: KeyGenerator
     #entityGenerator: EntityGenerator
     #mergeStrategy: MergeStrategy
+    #subscriptions = new Set<Subscription>()
     constructor(options: ManagerOptions) {
         this.#keyGenerator = options.keyGenerator
         this.#entityGenerator = options.entityGenerator || EntityManagerImpl.createEntityProxy
@@ -72,15 +80,33 @@ class EntityManagerImpl implements EntityManager {
         // Return the entity proxy
         return proxy
     }
-    merge<T extends object>(key: any, changes: Partial<T>): void {
-        // Try to get managed entity
-        const entity = this.#entities.get(key)
+    merge<T extends object>(entity: T, changes: Partial<T>): void {
+        // Generate the entity key
+        const key = this.#keyGenerator(entity)
+        // Get managed entity by key to unwrap proxies
+        const real = this.#entities.get(key)
         // Check if entity is attached
-        if (!entity) throw Error("Entity not attached")
+        if (!real) throw Error("Entity not attached")
         // Merge changes into managed entity
-        this.#mergeStrategy(this, entity, changes)
+        this.#mergeStrategy(this, real, changes)
         // Emit the "merge" event
         this.#merged.emit({ key, changes })
+    }
+    subscribe<T extends object>(entity: T): Subscription {
+        // Generate the entity key
+        const key = this.#keyGenerator(entity)
+        // Check if entity is attached
+        if (!this.#entities.has(key)) throw Error("Entity not attached")
+        // Generate an event subscription
+        const subscription = new SubscriptionImpl(this, key)
+        // Add the event subscription to subscriptions set
+        this.#subscriptions.add(subscription)
+        // Return the subscription object
+        return subscription
+    }
+    unsubscribe(subscription: Subscription) {
+        // Delete the event subscription from subscriptions set
+        if (!this.#subscriptions.delete(subscription)) throw Error("Illegal access")
     }
     private static createEntityProxy<T extends object>(manager: EntityManager, entity: T, key: any): T {
         const changeTracker = new ChangeTrackerImpl(manager, key)
@@ -97,5 +123,33 @@ class ChangeTrackerImpl<T extends object> implements ProxyHandler<T> {
     set(target: T, p: string | symbol, newValue: any, receiver: any): boolean {
         this.manager.merge(this.key, { [p]: newValue })
         return true
+    }
+}
+class SubscriptionImpl implements Subscription {
+    #detached = createEmitter("detached", this)
+    #merged = createEmitter("merged", this)
+    #detach: EventListener<DetachedEventData>
+    #merge: EventListener<MergedEventData>
+    constructor(readonly manager: EntityManager, readonly key: any) {
+        this.#merge = (ev: EventMessage<MergedEventData>) => {
+            if (ev.data.key == this.key) { 
+                this.#merged.emit(ev.data)
+            }
+        }
+        manager.merged.on(this.#merge)
+        this.#detach = (ev: EventMessage<DetachedEventData>) => {
+            if (ev.data.key == this.key) {
+                this.#detached.emit(ev.data)
+                this.unsubscribe()
+            }
+        }
+        manager.detached.on(this.#detach)
+    }
+    get detached() { return this.#detached }
+    get merged() { return this.#merged }
+    unsubscribe() {
+        this.manager.merged.off(this.#merge)
+        this.manager.detached.off(this.#detach)
+        this.manager.unsubscribe(this)
     }
 }
